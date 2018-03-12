27b138583673d4dee3007d6750860bb19f122d0f
Ricardo Tapia <rtapia92@gmail.com>
2018-03-11 21:07:26 -0600

    setup information and requirements

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
-from distutils.core import setup
+from setuptools import setup
+
+__version__ = '1.0.0'
+
 setup(
-  name = 'facturacom',
-  packages = ['facturacom'], # this must be the same as the name above
-  version = '0.1',
-  description = 'Integracion de factura.com',
-  author = 'Ricardo Tapia',
-  author_email = 'rtapia92@gmail.com',
-  url = 'https://github.com/richipargo/facturacom', # use the URL to the github repo
-  download_url = 'https://github.com/richipargo/facturacom/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
-  classifiers = [],
+    name='facturacom',
+    packages=['facturacom'], # this must be the same as the name above
+    version=__version__,
+    description='Integracion de factura.com',
+    author='Ricardo Tapia',
+    author_email='rtapia92@gmail.com',
+    url='https://github.com/richipargo/facturacom',
+    download_url='https://github.com/richipargo/facturacom/archive/1.0.0.tar.gz',
+    keywords=['testing', 'logging', 'example'], # arbitrary keywords
+    classifiers=[],
+    install_requires=[
+        'requests',
+    ]
 )
