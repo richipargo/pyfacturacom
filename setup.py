125cd89830061334ec446d282e4ff09073a615f9
Ricardo Tapia <rtapia92@gmail.com>
2020-02-24 20:46:34 +0100

    bump version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-__version__ = '1.1.0'
+__version__ = '1.2.0'
 
 setup(
     name='facturacom',
@@ -10,7 +10,7 @@ setup(
     author='Ricardo Tapia',
     author_email='rtapia92@gmail.com',
     url='https://github.com/richipargo/facturacom',
-    download_url='https://github.com/richipargo/facturacom/archive/1.0.0.tar.gz',
+    download_url='https://github.com/richipargo/facturacom/archive/1.2.0.tar.gz',
     keywords=['testing', 'logging', 'example'], # arbitrary keywords
     classifiers=[],
     install_requires=[
